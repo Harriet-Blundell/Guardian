5661d5e0d61a658604655c32fb6cbe112d4eb3b2
Harriet-Blundell <harriet.blundell27@hotmail.co.uk>
2020-05-07 17:54:55 +0100

    this commit will make CSS changes to the Header and add condition rendering to the previous button in Pagination to change the color if the current page is 1

--- Components/Pagination.js
+++ Components/Pagination.js
@@ -1,26 +1,84 @@
 import React from 'react'
-import { View, Button, Text } from 'react-native'
+import { View, Button, StyleSheet, Text, TouchableOpacity } from 'react-native'
 
 const Pagination = (props) => {
-  console.log(props.currentPage, 'i am here')
   const { currentPage, handlePageClick } = props
+
   return (
-    <View>
-      <Button
-        onPress={() => {
-          handlePageClick(1)
-        }}
-        title='Next'
-      />
-      <Button
+    <View style={styles.buttonsContainer}>
+      <TouchableOpacity
         disabled={currentPage === 1}
         onPress={() => {
           handlePageClick(-1)
         }}
-        title='Back'
-      />
+      >
+        <View
+          style={currentPage === 1 ? styles.buttonPrevOne : styles.buttonPrev}
+        >
+          <Text style={styles.buttonPrevText}>PREV</Text>
+        </View>
+      </TouchableOpacity>
+
+      <Text style={styles.pageNumber}>Page: {currentPage}</Text>
+
+      <TouchableOpacity
+        onPress={() => {
+          handlePageClick(1)
+        }}
+      >
+        <View style={styles.buttonNext}>
+          <Text style={styles.buttonNextText}>NEXT</Text>
+        </View>
+      </TouchableOpacity>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  buttonsContainer: {
+    margin: 20,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+
+  pageNumber: {
+    fontSize: 17,
+    marginTop: 8,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+
+  buttonPrev: {
+    width: 90,
+    borderRadius: 5,
+    alignItems: 'center',
+    backgroundColor: '#4682B4',
+  },
+
+  buttonPrevOne: {
+    backgroundColor: '#808080',
+    width: 90,
+    borderRadius: 5,
+    alignItems: 'center',
+  },
+
+  buttonPrevText: {
+    textAlign: 'center',
+    padding: 10,
+    color: 'white',
+  },
+
+  buttonNext: {
+    width: 90,
+    borderRadius: 5,
+    alignItems: 'center',
+    backgroundColor: '#4682B4',
+  },
+  buttonNextText: {
+    textAlign: 'center',
+    padding: 10,
+    color: 'white',
+  },
+})
 
 export default Pagination
