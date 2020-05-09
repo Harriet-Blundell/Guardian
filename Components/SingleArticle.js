2322b1a61e6fd81282a348d9fa2bd59e03294609
Harriet-Blundell <harriet.blundell27@hotmail.co.uk>
2020-05-09 14:58:11 +0100

    add api request in single Article to fetch article data by id

--- Components/SingleArticle.js
+++ Components/SingleArticle.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, ScrollView, Button, StyleSheet, Image } from 'react-native'
 import * as api from '../api.js'
+import { formatDate } from '../utils.js'
 
 class SingleArticle extends Component {
   state = {
@@ -8,13 +9,68 @@ class SingleArticle extends Component {
     isLoading: true,
   }
 
+  componentDidMount() {
+    const ArticleId = this.props.navigation.getParam('id')
+
+    api
+      .fetchSingleArticle(ArticleId, { 'show-fields': 'bodyText,thumbnail' })
+      .then(({ data }) => {
+        const { content } = data.response
+        this.setState({
+          singleArticle: content,
+          isLoading: false,
+        })
+      })
+  }
+
   render() {
-    return (
-      <View>
-        <Text>{this.props.navigation.getParam('apiUrl')}</Text>
-      </View>
-    )
+    const { singleArticle, isLoading } = this.state
+
+    {
+      !isLoading && console.log(singleArticle.fields)
+    }
+
+    return <View></View>
   }
 }
+
+const styles = StyleSheet.create({
+  singleArticleTitle: {
+    fontSize: 25,
+    fontWeight: 'bold',
+    paddingLeft: 20,
+    marginTop: 10,
+    marginBottom: 10,
+  },
+
+  singleArticleImage: {
+    width: '100%',
+    height: 260,
+    alignSelf: 'center',
+    marginBottom: 15,
+  },
+
+  singleArticleDate: {
+    marginLeft: 20,
+    fontSize: 18,
+    marginBottom: 10,
+    fontWeight: 'bold',
+  },
+
+  singleArticleSection: {
+    marginLeft: 10,
+    marginBottom: 10,
+    fontSize: 17,
+    color: '#4682B4',
+    fontWeight: 'bold',
+  },
+
+  singleArticleBodyText: {
+    fontSize: 20,
+    paddingLeft: 20,
+    paddingRight: 20,
+    marginBottom: 10,
+  },
+})
 
 export default SingleArticle
