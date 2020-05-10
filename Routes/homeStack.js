23720230fc4ee9ffa7ac382aa8ecbcf9ccc57d08
Harriet-Blundell <harriet.blundell27@hotmail.co.uk>
2020-05-10 19:45:56 +0100

    edit the position of the header on SingleArticle and ArticleList page because the back button has been implemented to the header again

--- Routes/homeStack.js
+++ Routes/homeStack.js
@@ -9,18 +9,39 @@ import React from 'react'
 const screens = {
   Home: {
     screen: Home,
+    navigationOptions: {
+      headerStyle: {
+        elevation: 10,
+      },
+    },
   },
   SingleArticle: {
     screen: SingleArticle,
+    navigationOptions: {
+      headerStyle: {
+        elevation: 10,
+      },
+      headerTitleContainerStyle: {
+        left: 25,
+      },
+    },
   },
   ArticleList: {
     screen: ArticleList,
+    navigationOptions: {
+      headerStyle: {
+        elevation: 10,
+      },
+      headerTitleContainerStyle: {
+        left: 25,
+      },
+    },
   },
 }
 
 const HomeStack = createStackNavigator(screens, {
   defaultNavigationOptions: {
-    headerTitle: () => <Header />,
+    headerTitle: (props) => <Header {...props} />,
   },
 })
 
