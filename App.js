ce650b0fa181ebb5881e4e35296a81078b9b083e
Harriet-Blundell <harriet.blundell27@hotmail.co.uk>
2020-05-10 15:41:05 +0100

    remove unnecessary code in App.js

--- App.js
+++ App.js
@@ -1,18 +1,8 @@
 import React from 'react'
-import { StyleSheet } from 'react-native'
 import Navigator from './Routes/homeStack.js'
 
 const App = () => {
   return <Navigator />
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-})
 
 export default App
