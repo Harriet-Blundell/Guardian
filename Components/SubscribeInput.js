26921e12216e5db15a1bdd84b5783a2bbd522d47
Harriet-Blundell <harriet.blundell27@hotmail.co.uk>
2020-05-10 11:31:13 +0100

    add a modal when the user clicks on the subscribe button and apply CSS for better UX

--- Components/SubscribeInput.js
+++ Components/SubscribeInput.js
@@ -6,11 +6,14 @@ import {
   StyleSheet,
   TouchableOpacity,
   Alert,
+  Modal,
+  TouchableHighlight,
 } from 'react-native'
 
 class Subscribe extends Component {
   state = {
     email: '',
+    showModal: false,
   }
 
   handleChange = (value) => {
@@ -33,34 +36,66 @@ class Subscribe extends Component {
       Alert.alert('You have successfully subscribed to the Guardian!'),
         this.setState({
           email: '',
+          showModal: false,
         })
     }
+  }
+
+  setModalVisible(value) {
+    this.setState({
+      showModal: value,
+    })
+  }
+
+  closeModal() {
+    this.setState({
+      showModal: false,
+    })
   }
 
   render() {
     const { email } = this.state
     return (
       <View style={styles.subscribeInputContainer}>
-        <Text
-          style={{
-            backgroundColor: '#191970',
-            textAlign: 'center',
-            paddingLeft: 70,
-            paddingRight: 70,
+        <Modal
+          transparent={true}
+          animationType='slide'
+          visible={this.state.showModal}
+          onRequestClose={() => {
+            this.closeModal()
           }}
         >
+          <View style={styles.subscribeInputContainer}>
+            <View style={styles.modalView}>
+              <TextInput
+                style={styles.textInput}
+                value={email}
+                onChangeText={(value) => this.handleChange(value)}
+                placeholder='Enter e-mail address'
+              />
+
+              <TouchableHighlight
+                onPress={() => {
+                  this.setModalVisible(!this.state.showModal)
+                }}
+              >
+                <TouchableOpacity onPress={() => this.handleSubmit()}>
+                  <View style={styles.submitBtnContainer}>
+                    <Text style={styles.submitBtnText}>SUBMIT</Text>
+                  </View>
+                </TouchableOpacity>
+              </TouchableHighlight>
+            </View>
+          </View>
+        </Modal>
+
+        <Text style={styles.subscribeHeaderContainer}>
           <Text style={styles.subscribeText}>
             Subscribe to get updates on the latest news...
           </Text>
         </Text>
 
-        <TextInput
-          style={styles.textInput}
-          value={email}
-          onChangeText={(value) => this.handleChange(value)}
-          placeholder='Enter e-mail address'
-        />
-        <TouchableOpacity onPress={() => this.handleSubmit()}>
+        <TouchableOpacity onPress={() => this.setModalVisible(true)}>
           <View style={styles.subscribeBtnContainer}>
             <Text style={styles.subscribeBtnText}>SUBSCRIBE</Text>
           </View>
@@ -72,7 +107,33 @@ class Subscribe extends Component {
 
 const styles = StyleSheet.create({
   subscribeInputContainer: {
-    backgroundColor: 'whitesmoke',
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 22,
+  },
+
+  subscribeHeaderContainer: {
+    backgroundColor: '#191970',
+    textAlign: 'center',
+    paddingLeft: 70,
+    paddingRight: 70,
+    marginBottom: 10,
+  },
+
+  modalView: {
+    margin: 20,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 35,
+    height: 200,
+    width: 350,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
   },
 
   subscribeText: {
@@ -97,17 +158,31 @@ const styles = StyleSheet.create({
 
   subscribeBtnContainer: {
     justifyContent: 'center',
-    marginTop: 10,
-    width: 100,
+    width: 150,
     height: 35,
     borderRadius: 5,
     backgroundColor: 'black',
-    marginLeft: 150,
-    marginBottom: 5,
+    marginBottom: 10,
     elevation: 5,
   },
 
   subscribeBtnText: {
+    color: 'white',
+    textAlign: 'center',
+    fontWeight: 'bold',
+  },
+
+  submitBtnContainer: {
+    justifyContent: 'center',
+    width: 150,
+    height: 35,
+    borderRadius: 5,
+    backgroundColor: '#4682B4',
+    marginTop: 40,
+    elevation: 5,
+  },
+
+  submitBtnText: {
     color: 'white',
     textAlign: 'center',
     fontWeight: 'bold',
